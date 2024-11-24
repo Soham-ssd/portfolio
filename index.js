443e021ba7653a7abdb05d0f1ae75b6721b37dd4
Soham Dabadgaonkar <130518761+Soham-ssd@users.noreply.github.com>
2024-11-24 13:40:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter(){
-    let response = await fetch("https://nk6infduov4i4r7uc52efv22jy0vyvqj.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://z3rixbxrko46rfwbbd7tn2gxhi0lkzjq.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Total Views: ${data}`;
 
